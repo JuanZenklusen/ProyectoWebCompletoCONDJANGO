5b886330910d83cc31b17e636c85e408823cb8f0
JuanZenklusen <101181838+JuanZenklusen@users.noreply.github.com>
2023-01-11 23:52:36 -0300

    First commit

---
+++ ProyectoWeb/proyectowebapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, HttpResponse
+
+def home(request):
+    return render(request, 'proyectowebapp/home.html')
+
+def servicios(request):
+    return render(request, 'proyectowebapp/servicios.html')
+
+def tienda(request):
+    return render(request, 'proyectowebapp/tienda.html')
+
+def blog(request):
+    return render(request, 'proyectowebapp/blog.html')
+
+def contacto(request):
+    return render(request, 'proyectowebapp/contacto.html')
